a7d03ee39a40d5161eed3533550c19f87820a857
James Cooper <74224020+n0dat@users.noreply.github.com>
2022-03-07 19:08:53 -0700

    mmh

--- Assignment 34/test.py
+++ Assignment 34/test.py
@@ -268,3 +268,49 @@ def mmd(state, game):
 		return v
 	# body of minmax_decision:
 	return max(game.actions(state), key = lambda a: min_value(game.result(a)))
+
+
+
+
+def abs(state, game):
+    """Search game to determine best action; use alpha-beta pruning.
+    As in [Figure 5.7], this version searches all the way to the leaves."""
+
+    player = game.to_move(state)
+
+    # Functions used by alpha_beta
+    def max_value(state, alpha, beta):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = -np.inf
+        tmp = copy.copy(game.actions(state))
+        for a in tmp:
+            v = max(v, min_value(game.result(a), alpha, beta))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+
+    def min_value(state, alpha, beta):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = np.inf
+        tmp = copy.copy(game.actions(state))
+        for a in tmp:
+            v = min(v, max_value(game.result(a), alpha, beta))
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+        return v
+
+    # Body of alpha_beta_search:
+    best_score = -np.inf
+    beta = np.inf
+    best_action = None
+    tmp = copy.copy(game.actions(state))
+    for a in tmp:
+        v = min_value(game.result(a), best_score, beta)
+        if v > best_score:
+            best_score = v
+            best_action = a
+    return best_action
